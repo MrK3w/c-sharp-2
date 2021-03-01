adf94b58df4e7ff50cfb96436e2e761482d32b76
MrK3w <jakubpietrus2000@gmail.com>
2021-03-01 17:29:42 +0100

    added lesson2

---
+++ Threads/Synchronization/Program.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.Design;
+using System.Threading;
+using Threads;
+
+namespace Synchronization
+{
+    class Program
+    {
+        private static object lockObject = new object();
+        static void Main(string[] args)
+        {
+            Random rnd = new Random();
+            List<Thread> threads = new List<Thread>();
+            SimpleStack<int> stack = new SimpleStack<int>();
+            Thread t1 = new Thread(() =>
+            {
+                while (true)
+                {
+                    stack.Push(rnd.Next());
+                    Thread.Sleep(100);
+                }
+            });
+            t1.Start();
+
+            for (int i = 0; i < 5; i++)
+            {
+                Thread t2 = new Thread(() =>
+                {
+                    while (true)
+                    {
+                        lock (lockObject)
+                        {
+                            if (stack.IsEmpty)
+                            {
+                                continue;
+                            }
+                        }
+
+                        lock (lockObject)
+                        {
+                            int val = stack.Pop();
+                        
+                        Console.WriteLine($"Hodnota: {val} | Vlakno: {Thread.CurrentThread.ManagedThreadId}");
+                        }
+
+                        Thread.Sleep(rnd.Next(40,1001));
+                    }
+                });
+                t2.Start();
+                threads.Add(t2);
+            }
+            t1.Join();
+            foreach (var t in threads)
+            {
+                t.Join();
+            }
+        }
+    }
+}
---
+++ Threads/Threads/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading;
+
+namespace Threads
+{
+    class Program
+    {
+        private static SimpleStack<int> stack = new SimpleStack<int>();
+        private static object lockObject = new object();
+
+        static void Main()
+        {
+            Random rand = new Random();
+            List<Thread> threads = new List<Thread>();
+            for (int i = 0; i < 5; i++)
+            {
+                Thread t = new Thread(Execute);
+                t.IsBackground = true;
+                t.Start(rand);
+                threads.Add(t);
+            }
+
+            foreach (Thread thread in threads)
+            {
+                thread.Join();
+            }
+        }
+
+        public static void Execute(object randomGenerator)
+        {
+            Random rand = (Random)randomGenerator;
+            while (true)
+            {
+
+                if (rand.NextDouble() > 0.6)
+                {
+                    stack.Push(rand.Next());
+                }
+                else
+                {
+                    lock (lockObject)
+                    {
+                        if (!stack.IsEmpty)
+                        {
+                            stack.Pop();
+                        }
+                    }
+                }
+            }
+        }
+    }
+}
